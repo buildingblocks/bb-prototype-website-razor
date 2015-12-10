74a95d1b5408d614353e9320ec1b59cebcb02002
Jordan Robinson <j.robinson@building-blocks.com>
2015-12-10 14:45:05 +0000

    Adds model helper and various examples

---
+++ Helpers/ModelHelper.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Mvc.Rendering;
+using Newtonsoft.Json.Linq;
+
+namespace Helpers
+{
+    public static class ModelHelper
+    {
+        private readonly static string ModelPath = "data\\";
+        
+        public static dynamic GetModel(string JSONFile)
+        {
+            return JObject.Parse(File.ReadAllText(ModelPath + JSONFile));
+        }
+    }
+}

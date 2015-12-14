c0d8d7481fe60c9be448761caff3a00f276f0423
Jordan Robinson <hypahnova@gmail.com>
2015-12-14 19:47:14 +0000

    Small fix for the model helper

--- Helpers/ModelHelper.cs
+++ Helpers/ModelHelper.cs
@@ -1,11 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using Microsoft.AspNet.Mvc;
-using Microsoft.AspNet.Mvc.Rendering;
 using Newtonsoft.Json.Linq;
+using System.IO;
 
 namespace Helpers
 {
@@ -18,7 +12,7 @@ namespace Helpers
             return JObject.Parse(File.ReadAllText(ModelPath + JSONFile));
         }
 
-        public static bool Exists(this dynamic Object, string property) {
+        public static bool Exists(dynamic Object, string property) {
             return Object.GetType().GetProperty(property) != null;
         }
     }

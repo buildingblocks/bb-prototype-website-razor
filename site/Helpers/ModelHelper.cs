07274b465d1a9cf7007c3c735322c1115ef47b64
Jordan Robinson <j.robinson@building-blocks.com>
2017-01-13 12:50:04 +0000

    Upgrading .net core

--- site/Helpers/ModelHelper.cs
+++ site/Helpers/ModelHelper.cs
@@ -5,11 +5,11 @@ namespace Helpers
 {
     public static class ModelHelper
     {
-        private readonly static string ModelPath = "data/";
+        private static readonly string _modelPath = "data/";
 
         public static dynamic GetModel(string JSONFile)
         {
-            return JObject.Parse(File.ReadAllText(ModelPath + JSONFile));
+            return JObject.Parse(File.ReadAllText(_modelPath + JSONFile));
         }
 
         public static bool Exists(dynamic Object, string property) {
--- site/Startup.cs
+++ site/Startup.cs
@@ -1,48 +1,27 @@
-
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNet.Builder;
-using Microsoft.AspNet.Hosting;
-using Microsoft.AspNet.Mvc.Razor;
-using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc;
 
 namespace bb_prototype_website_razor
 {
     public class Startup
     {
-        public Startup(IHostingEnvironment env)
-        {
-            // Set up configuration sources.
-            var builder = new ConfigurationBuilder()
-                .AddJsonFile("appsettings.json")
-                .AddEnvironmentVariables();
-            Configuration = builder.Build();
-        }
-
-        public IConfigurationRoot Configuration { get; set; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            // Add framework services.
+            services.AddOptions();
             services.AddMvc();
-            services.Configure<RazorViewEngineOptions>(options =>
-            {
-                var expander = new LayoutLocationExpander();
-                options.ViewLocationExpanders.Add(expander);
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
+            loggerFactory.AddConsole();
+            
 
             if (env.IsDevelopment())
             {
@@ -53,19 +32,22 @@ namespace bb_prototype_website_razor
                 app.UseExceptionHandler("/Layouts/Error");
             }
 
-            app.UseIISPlatformHandler();
-
-            app.UseStaticFiles();
-
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Pages}/{action=Index}/{id?}");
             });
-        }
 
-        // Entry point for the application.
-        public static void Main(string[] args) => Microsoft.AspNet.Hosting.WebApplication.Run<Startup>(args);
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
     }
 }

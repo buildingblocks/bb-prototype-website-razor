fbb275cc801ff0ec6727dd46f8c88a9908d27dbf
Chris Eccles <c.eccles@building-blocks.com>
2017-01-13 12:16:55 +0000

    Moving the .net core files to a sub folder "site"

---
+++ site/Controllers/PagesController.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace bb_prototype_website_razor.Controllers
+{
+    public class PagesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("~/Views/Pages/Index.cshtml");
+        }
+
+        public IActionResult LayoutA()
+        {
+            return View();
+        }
+
+        public IActionResult LayoutB()
+        {
+            return View();
+        }
+
+        public IActionResult Grid()
+        {
+            return View();
+        }
+
+        public IActionResult Styleguide()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}

ad04dc82751d1e4507a0c70e2578334316352bd4
Rob Stevenson-Leggett <r.leggett@building-blocks.com>
2017-01-14 18:21:25 +0000

    Trying to get ViewComponents to work

---
+++ Extensions/HtmlHelperExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Helpers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace bb_prototype_website_razor.Extensions
+{
+    public static class HtmlHelperExtensions
+    {
+        public static bool Exists(this IHtmlHelper<dynamic> helper, string propertyName)
+        {
+            return ModelHelper.Exists(helper.ViewData.Model, propertyName);
+        }
+    }
+}
---
+++ ViewComponents/JsonModelDrivenViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Helpers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bb_prototype_website_razor.Controllers
+{
+    public class JsonModelDrivenViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(string viewName, string componentPath)
+        {
+            string componentFile = $"{componentPath.Split('.')[0]}.json";
+            string path = componentPath.Split('.')[1];
+            var model = ModelHelper.GetModel(componentFile, path);
+            return View(viewName, model);
+        }
+    }
+}
